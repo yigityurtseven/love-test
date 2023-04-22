b9dafb8c931f9bce0d7404d53440ad40f9f137a8
Yigit Yurtseven <y.yurtseven@outlook.com>
2023-04-22 15:08:18 +0300

    icon changed

--- love-test/vite.config.js
+++ love-test/vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/love-test",
   plugins: [vue()],
   server: {
     host: true,
